d27d7543fe1da553a2950700501d5ec4e9e82f9f
zhaohongxuan <hxzhenu@gmail.com>
2022-05-20 18:50:14 +0800

    fix: book category error

--- src/utils/frontmatter.ts
+++ src/utils/frontmatter.ts
@@ -5,7 +5,6 @@ type FrontMatterContent = {
 	doc_type: string;
 	bookId: string;
 	noteCount: number;
-	category: string;
 	reviewCount: number;
 };
 
@@ -15,7 +14,6 @@ export const addFrontMatter = (markdownContent: string, noteBook: Notebook) => {
 	const frontMatter: FrontMatterContent = {
 		doc_type: frontMatterDocType,
 		bookId: noteBook.metaData.bookId,
-		category: noteBook.metaData.category,
 		reviewCount: noteBook.metaData.reviewCount,
 		noteCount: noteBook.metaData.noteCount
 	};
