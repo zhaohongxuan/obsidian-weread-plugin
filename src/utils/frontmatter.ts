6e2a069e09713aeecb692ddd9003d9fd15075e06
zhaohongxuan <hxzhenu@gmail.com>
2022-05-29 14:06:03 +0800

    customize front matter

--- src/utils/frontmatter.ts
+++ src/utils/frontmatter.ts
@@ -4,6 +4,8 @@ import type { Notebook } from '../models';
 type FrontMatterContent = {
 	doc_type: string;
 	bookId: string;
+	author: string;
+	cover:string
 	noteCount: number;
 	reviewCount: number;
 };
@@ -14,9 +16,25 @@ export const addFrontMatter = (markdownContent: string, noteBook: Notebook) => {
 	const frontMatter: FrontMatterContent = {
 		doc_type: frontMatterDocType,
 		bookId: noteBook.metaData.bookId,
+		author:noteBook.metaData.author,
+		cover:noteBook.metaData.cover,
 		reviewCount: noteBook.metaData.reviewCount,
 		noteCount: noteBook.metaData.noteCount
 	};
 
 	return matter.stringify(markdownContent, frontMatter);
+};
+
+export const updateFrontMatter = (markdownContent: string, noteBook: Notebook,existFileContent:string) => {
+	const frontMatter: FrontMatterContent = {
+		doc_type: frontMatterDocType,
+		bookId: noteBook.metaData.bookId,
+		author:noteBook.metaData.author,
+		cover:noteBook.metaData.cover,
+		reviewCount: noteBook.metaData.reviewCount,
+		noteCount: noteBook.metaData.noteCount
+	};
+	const existFrontMatter = matter(existFileContent);
+	const freshFrontMatter = {...existFrontMatter.data,...frontMatter};
+	return matter.stringify(markdownContent, freshFrontMatter);
 };
