ebe6284ca469337712270b73ac38be761cad773d
zhaohongxuan <hxzhenu@gmail.com>
2025-03-28 08:28:24 +0800

    fix: replace function enhancement;
    update

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,28 +5,31 @@ import { get } from 'svelte/store';
 export class Renderer {
 	constructor() {
 		nunjucks.configure({ autoescape: false })
-		// 自定义函数 https://mozilla.github.io/nunjucks/api.html#addfilter
-		.addFilter('replace', function(str, pattern, replacement) {
-			if (typeof pattern === 'string') {
-				try {
-					// 如果 pattern 以 /.../ 开头和结尾，解析为正则表达式
-					if (pattern.startsWith('/') && pattern.lastIndexOf('/') > 0) {
-						const regexBody = pattern.slice(1, pattern.lastIndexOf('/'));
-						const flags = pattern.slice(pattern.lastIndexOf('/') + 1);
-						pattern = new RegExp(regexBody, flags);
-					} else {
-						return str.replace(pattern, replacement);
+			// 自定义函数 https://mozilla.github.io/nunjucks/api.html#addfilter
+			.addFilter('replace', function (str, pattern, replacement) {
+				if (!str)
+					return ''
+
+				if (typeof pattern === 'string') {
+					try {
+						// 如果 pattern 以 /.../ 开头和结尾，解析为正则表达式
+						if (pattern.startsWith('/') && pattern.lastIndexOf('/') > 0) {
+							const regexBody = pattern.slice(1, pattern.lastIndexOf('/'));
+							const flags = pattern.slice(pattern.lastIndexOf('/') + 1);
+							pattern = new RegExp(regexBody, flags);
+						} else {
+							return str.replaceAll(pattern, replacement);
+						}
+					} catch (e) {
+						// 如果正则表达式无效，回退到字符串替换
+						return str.replaceAll(pattern, replacement);
 					}
-				} catch (e) {
-					// 如果正则表达式无效，回退到字符串替换
-					console.log("replace error", e)
+				} else if (pattern instanceof RegExp) {
 					return str.replace(pattern, replacement);
 				}
-			}
-			// 如果 pattern 是正则表达式，直接使用
-			return str.replace(pattern, replacement);
-		  })
-		  ;
+				return str.replaceAll(pattern, replacement);
+			})
+			;
 	}
 
 	validate(template: string): boolean {
